dbc77e2433381e8b24cad232c989f1f6684ad0bd
equal-l2 <equal-l2@users.noreply.github.com>
2016-12-19 01:44:42 +0900

    Adjust indent

--- make_histo.cpp
+++ make_histo.cpp
@@ -8,46 +8,46 @@
 
 template <typename T>
 T ato(char* a){
-    std::stringstream s;
-    s << a;
-    T buf;
-    s >> buf;
-    return buf;
+  std::stringstream s;
+  s << a;
+  T buf;
+  s >> buf;
+  return buf;
 }
 
 int main(int argc, char** argv){
-    std::ios::sync_with_stdio(false);
+  std::ios::sync_with_stdio(false);
 
-    if(argc <= 4){
-	std::cout << "Usage: make_histo <number of bin> <min of histo> <max of histo> <filename>\n";
-	std::cout << "Note: If filename is \"-\", input data will be received from stdin\n";
-	return 1;
-    }
+  if(argc <= 4){
+    std::cout << "Usage: make_histo <number of bin> <min of histo> <max of histo> <filename>\n";
+    std::cout << "Note: If filename is \"-\", input data will be received from stdin\n";
+    return 1;
+  }
 
-    const auto n_of_bin = ato<unsigned long long>(argv[1]);
-    const auto min = atof(argv[2]);
-    const auto max = atof(argv[3]);
-    const std::string filename(argv[4]);
-    
-    Histo h(min,max,n_of_bin);
-    
-    std::shared_ptr<std::istream> is;
-    if(filename == "-"){
-	is.reset(&std::cin,[](...){});
-    }
-    else{
-	is.reset(new std::ifstream(filename));
-    }
+  const auto n_of_bin = ato<unsigned long long>(argv[1]);
+  const auto min = atof(argv[2]);
+  const auto max = atof(argv[3]);
+  const std::string filename(argv[4]);
 
-    if(!(*is)){
-	std::cout << "Error on opening \"" << filename << "\"\n";
-	return 1;
-    }
+  Histo h(min,max,n_of_bin);
 
-    double buf;
-    while(*is >> buf){
-	h.insert(buf);
-    }
+  std::shared_ptr<std::istream> is;
+  if(filename == "-"){
+    is.reset(&std::cin,[](...){});
+  }
+  else{
+    is.reset(new std::ifstream(filename));
+  }
 
-    h.print(std::cout);
+  if(!(*is)){
+    std::cout << "Error on opening \"" << filename << "\"\n";
+    return 1;
+  }
+
+  double buf;
+  while(*is >> buf){
+    h.insert(buf);
+  }
+
+  h.print(std::cout);
 }
--- particle.hpp
+++ particle.hpp
@@ -8,29 +8,29 @@
 
 template<unsigned dim>
 struct particle{
-    particle():r(),v(){}
-    std::array<double,dim> r;
-    std::array<double,dim> v;
+  particle():r(),v(){}
+  std::array<double,dim> r;
+  std::array<double,dim> v;
 };
 
 struct particles : public std::array<particle<dim>,ptclnum> {
-    void adv(){
-        static auto v2 = std::bind(
-            std::normal_distribution<>(0,sqrt((k*T/m)*(1-std::exp(-2*beta*dt)))),
-            #if FIXED_SEED
-                std::mt19937(0)
-            #else
-                std::mt19937(std::chrono::system_clock::now().time_since_epoch().count())
-            #endif
-        );
+  void adv(){
+    static auto v2 = std::bind(
+      std::normal_distribution<>(0,sqrt((k*T/m)*(1-std::exp(-2*beta*dt)))),
+      #if FIXED_SEED
+      std::mt19937(0)
+      #else
+      std::mt19937(std::chrono::system_clock::now().time_since_epoch().count())
+      #endif
+    );
 
-        static const double c1 = 1-std::exp(-beta*dt);
+    static const double c1 = 1-std::exp(-beta*dt);
 
-        for(auto& p : *this){
-            for(unsigned i=0;i<dim;++i){
-                p.r[i] += c1*p.v[i]/beta;
-                p.v[i] += -p.v[i]*c1+v2();
-            }
-        }
+    for(auto& p : *this){
+      for(unsigned i=0;i<dim;++i){
+        p.r[i] += c1*p.v[i]/beta;
+        p.v[i] += -p.v[i]*c1+v2();
+      }
     }
+  }
 };
